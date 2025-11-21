15122bf9c761332e80395668c8276910399414f9
Harold Areiza <haroldalepieare@gmail.com>
2025-11-20 21:08:26 -0500

    Add project files.

---
+++ APIMovies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMovies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] //  Data anotation indica que el campo es obligatorio 
+        [Display(Name = "Nombre de Categoria")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
